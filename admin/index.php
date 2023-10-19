f4cbadd6f53775501ba89fe664806983247ecb9a
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-19 15:33:19 +0300

    First commit

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,32 @@
-<?php
+<?php include('partials/menu.php') ?>
+
+      <div class="main_content">
+      <div class="wrapper">
+        <h1>DASHBOARD</h1>
+
+        <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Category
+        </div>
+
+        <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Category
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Category
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1> <br>
+          Category
+        </div>
+
+        <div class="clearfix "></div>
 
 
+       </div>
 
-?>
+      </div>
+      
+      <?php include('partials/footer.php') ?>
