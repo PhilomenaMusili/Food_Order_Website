7262b94228b7981d6b7567c2dc9ab33ea5b958a4
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-19 15:34:36 +0300

    first commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+          <div class="wrapper">
+            <p class="text-center">2020 All rights reserved, Food House. Developed By -<a href=""> Philomena Kyalo</a></p>
+
+        </div>
+      </div>
+   </body>
+</html>
