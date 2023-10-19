7ca9f70f278e764dc76c63d3feca4ba2b82c4f86
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-19 15:34:11 +0300

    First commit

---
+++ admin/manage-category.php
@@ -0,0 +1,8 @@
+<?php include('partials/menu.php') ?>
+
+<div class="main_content">
+    <div class="wrapper">
+        <h1> manage category</h1>
+    </div>
+</div>
+<?php include('partials/footer.php') ?>
