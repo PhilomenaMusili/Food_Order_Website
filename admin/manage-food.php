b2a1d68665c1c3724f23feb81cf0e6f44ce9f075
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-19 15:34:26 +0300

    first commit

---
+++ admin/manage-food.php
@@ -0,0 +1,8 @@
+<?php include('partials/menu.php') ?>
+
+<div class="main_content">
+    <div class="wrapper">
+        <h1> Manage Food</h1>
+    </div>
+</div>
+<?php include('partials/footer.php') ?>
