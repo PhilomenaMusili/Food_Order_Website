c43ca78fb7cee7fe095d3b696203c7b2352d7bb8
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-19 15:33:42 +0300

    first commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,10 @@
+<?php include('partials/menu.php'); ?>
+
+      <div class="main_content">
+      <div class="wrapper">
+        <h1>Manage Admin</h1>
+
+        
+
+      </div>
+      <?php include('partials/footer.php'); ?>
