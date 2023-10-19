7b4d7c53d4e18e4417ed671128c0429ec370ba65
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-19 15:34:52 +0300

    First Commit

---
+++ admin/partials/menu.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Food Order Website</title>
+   </head>
+
+   <body>
+      <div class="menu text-center">
+        <div class="wrapper">
+          <ul>
+           <li><a href="index.php">Home</a></li> 
+           <li><a href="manage-admin.php">Admin Manager</a></li> 
+           <li><a href="manage-category.php">Category</a></li> 
+           <li><a href="manage-food.php">Food</a></li> 
+           <li><a href="manage-order.php">Order</a></li> 
+          </ul>
+
+        </div>
+
+      </div>
