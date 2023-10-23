89e72c07e6add0183f597955901cf35a5c2dc6a8
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-23 15:40:02 +0300

    Third Commit

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,23 +4,25 @@
       <div class="wrapper">
         <h1>Manage Admin</h1>
         
-        <table class="tbl-full">
+        <table class="table">
+
          <tr>
           <th>S.N</th>
           <th>Full name</th>
           <th>Username</th>
           <th>Actions</th>
          </tr> 
+
          <tr>
           <td>1</td>
           <td>Mina</td>
           <td>Phil</td>
           <td>
             Update Admin
-            Delete Adnin
+            Delete Admin
           </td>
          
-        </tr>
+         </tr>
         </table>
 
         
