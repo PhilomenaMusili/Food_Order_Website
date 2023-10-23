5d69faef65dcd1a59747d12c4a200786bb864a2b
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-23 13:06:58 +0300

    Second Commit

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,6 +3,25 @@
       <div class="main_content">
       <div class="wrapper">
         <h1>Manage Admin</h1>
+        
+        <table class="tbl-full">
+         <tr>
+          <th>S.N</th>
+          <th>Full name</th>
+          <th>Username</th>
+          <th>Actions</th>
+         </tr> 
+         <tr>
+          <td>1</td>
+          <td>Mina</td>
+          <td>Phil</td>
+          <td>
+            Update Admin
+            Delete Adnin
+          </td>
+         
+        </tr>
+        </table>
 
         
 
