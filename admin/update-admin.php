d0851a71c76edf0673db49c9e99ce5af716a37be
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-30 14:11:28 +0300

    Done With update button

--- admin/update-admin.php
+++ admin/update-admin.php
@@ -58,6 +58,7 @@ if($res==true){
             <tr>
                 
                 <td colspan="2">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
                     <input type="submit" name="submit" value="Update Admin" class="btn-secondary">
                 </td>
             </tr>
@@ -72,6 +73,37 @@ if($res==true){
  if(isset($_POST['submit']))
  {
     //echo "button clicked";
+    //get all values from form to update
+        $id= $_POST['id'];
+        $full_name= $_POST['full_name'];
+        $username= $_POST['username'];
+    //create sql query to update
+    $sql = "UPDATE tbl_admin SET full_name ='$full_name',
+    username= '$username'
+    WHERE id ='$id'
+    ";
+
+    //execute the query
+    $res=mysqli_query($conn, $sql);
+
+    //check if query is executed sccessfuly
+    if($res==true){
+        $_SESSION['update']="<div class='success'>Admin updated Successfuly.</div>";
+        
+    //redirect to manage admin page
+    header('location:'.SITEURL.'admin/manage-admin.php');
+
+
+    }
+    else {
+        $_SESSION['update']="<div class='success'>Failed to Update.</div>";
+        
+        //redirect to manage admin page
+        header('location:'.SITEURL.'admin/manage-admin.php');
+    
+    
+
+    }
  }
 ?>
 
