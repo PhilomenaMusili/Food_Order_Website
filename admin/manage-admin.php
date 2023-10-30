dd63718a3722c5f8abfd8f2e2cb60908616ce55c
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-30 14:16:32 +0300

    Added Password Button

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -70,6 +70,7 @@
                 <td><?php echo $full_name; ?></td>
                 <td><?php echo $username; ?></td>
                 <td>
+                  <a class="btn-primary">Change Password </a>
                 <a href="<?php echo SITEURL; ?>admin/update-admin.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
                 <a href="<?php echo SITEURL; ?>admin/delete-admin.php?id=<?php echo $id; ?>" class="btn-danger">Delete Admin</a>
                 </td>
