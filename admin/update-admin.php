0224a8a1bb48b61ab9f2e43f4f35b7bf1553178a
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-30 12:57:14 +0300

    Created Update-admin.php page

---
+++ admin/update-admin.php
@@ -0,0 +1,9 @@
+<?php include('partials/menu.php') ?>
+
+
+<div class="main_content">
+    <div class="wrapper">
+    <h1>Update Admin</h1>
+    </div>
+</div>
+<?php include('partials/footer.php') ?>
