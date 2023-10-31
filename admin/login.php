749d43e2483ca27948a0403104cd313d229f1ad5
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-31 10:38:21 +0300

    Second Commit

--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,12 @@
-<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <div class="login"></div>
+</body>
+</html>
