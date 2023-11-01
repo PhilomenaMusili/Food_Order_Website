7eae3a8b082c8d603adf97c12655873b6777358e
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-01 11:36:33 +0300

    Another Commit

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -5,7 +5,7 @@
         <h1> Manage Category</h1>
 
         <br><br><br>
-        <a href="#" class="btn-primary">Add Category</a>
+        <a href="<?php echo SITEURL; ?>admin/add-category.php" class="btn-primary">Add Category</a>
         <br><br><br>
         
         <table class="table">
