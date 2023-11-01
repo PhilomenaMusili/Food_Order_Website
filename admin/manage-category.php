5fb55eaba421f15822150f4728da84a172dfd722
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-01 12:39:57 +0300

    Another Commit

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -5,6 +5,17 @@
         <h1> Manage Category</h1>
 
         <br><br><br>
+        <?php
+        
+        if(isset($_SESSION['add']))
+        {
+         echo $_SESSION['add'];
+        unset($_SESSION['add']);
+
+        }
+
+        ?>
+        <br><br>
         <a href="<?php echo SITEURL; ?>admin/add-category.php" class="btn-primary">Add Category</a>
         <br><br><br>
         
