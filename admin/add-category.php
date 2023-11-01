291b799e43fddccc99c697ee6eaf8dca72df3cef
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-01 12:09:31 +0300

    another commit

--- admin/add-category.php
+++ admin/add-category.php
@@ -41,6 +41,55 @@
                 </tr>
             </table>
         </form>
+        <?php
+        //check if submit buttom is clicked
+        if(isset($_POST['submit']))
+        {
+            $title= $_POST['title'];
+
+            if(isset($_POST['featured'])) 
+            {
+                $featured= $_POST['featured'];
+
+            }
+            else 
+            {
+                $featured="No";
+
+            }
+
+            if(isset($_POST['active'])) 
+            {
+                $active= $_POST['active'];
+
+            }
+            else 
+            {
+                $active="No";
+
+            }
+            //sql to insert category to db
+            $sql="INSERT INTO tbl_category SET
+               title='$title',
+               featured='$featured',
+               active='$active'
+            ";
+
+            //execute query
+            $res=mysqli_query($conn,$sql);
+
+            //check out if querry is executed or not
+
+            if($res==true){
+
+            }
+            else{
+                //faile to execute query
+            }
+
+
+        }
+        ?>
 
     </div>
 </div>
