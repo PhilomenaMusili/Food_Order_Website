ff9bca8d22e8c05f1d1635c3b6668b188f1d888f
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-01 13:43:48 +0300

    Another Commit

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -23,40 +23,54 @@
 
          <tr>
           <th>S.N</th>
-          <th>Full name</th>
-          <th>Username</th>
+          <th>Title</th>
+          <th>Image</th>
+          <th>Featured</th>
+          <th>Active</th>
           <th>Actions</th>
          </tr> 
+         <?php
+          $sql="SELECT * FROM tbl_category";
 
-         <tr>
-          <td>1</td>
-          <td>Mina</td>
-          <td>Phil</td>
-          <td>
-          <a href="#" class="btn-secondary">Update Admin</a>
-          <a href="#" class="btn-danger">Delete Admin</a>
-          </td>
-         </tr>
+          $res= mysqli_query($conn, $sql);
 
-         <tr>
-          <td>2</td>
-          <td>Mina</td>
-          <td>Phil</td>
-          <td>
-          <a href="#" class="btn-secondary">Update Admin</a>
-          <a href="#" class="btn-danger">Delete Admin</a>
-          </td>
-         </tr>
+          $count = mysqli_num_rows($res);
 
-         <tr>
-          <td>3</td>
-          <td>Mina</td>
-          <td>Phil</td>
-          <td>
-          <a href="#" class="btn-secondary">Update Admin</a>
-          <a href="#" class="btn-danger">Delete Admin</a>
-          </td>
-         </tr>
+          if($count>0) {
+            while($row=mysqli_fetch_assoc($res))
+            {
+                $id = $row['id'];
+                $title = $row['title'];
+                $image_name = $row['image_name'];
+                $featured = $row['featured'];
+                $active = $row['active'];
+                ?>
+
+          <tr>
+            <td>1</td>
+            <td><?php echo $title; ?></td>
+            <td><?php echo $image_name; ?></td>
+            <td><?php echo $featured; ?></td>
+            <td><?php echo $active; ?></td>
+            <td>
+            <a href="#" class="btn-secondary">Update Category</a>
+            <a href="#" class="btn-danger">Delete Category</a>
+            </td>
+          </tr>
+                <?php
+            }
+
+          }
+          else {
+            ?>
+            <tr>
+                <td colspan="6"><div class="error">No category Added.</div></td>
+            </tr>
+            <?php
+          }
+         ?>
+
+         
         </table>
     </div>
 </div>
