91902bf79abd084024715d845009dc5242b5ca54
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-02 11:48:16 +0300

    delete

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -13,6 +13,18 @@
         unset($_SESSION['add']);
 
         }
+        if(isset($_SESSION['remove']))
+        {
+         echo $_SESSION['remove'];
+        unset($_SESSION['remove']);
+
+        }
+        if(isset($_SESSION['delete']))
+        {
+         echo $_SESSION['delete'];
+        unset($_SESSION['delete']);
+
+        }
 
         ?>
         <br><br>
