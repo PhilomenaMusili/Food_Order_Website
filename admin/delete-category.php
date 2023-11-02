fd2f14658c47d330918d1a247b210c98b37cd4e3
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-02 11:49:22 +0300

    donr with delete button

--- admin/delete-category.php
+++ admin/delete-category.php
@@ -1,38 +1,39 @@
 <?php
 include('../config/constants.php');
 
-//check if id and img name have been passed
+// Check if 'id' and 'image_name' have been passed
+if (isset($_GET['id']) && isset($_GET['image_name'])) {
+    $id = $_GET['id'];
+    $image_name = $_GET['image_name'];
 
-if(isset($_GET['id']) AND isset($_GET['image_name']))
-{
-   //
-   $id = $_GET['image_name'];
-   $image_name = $_GET['image_name'];
+    if ($image_name != "") {
+        $path = "../images/category/" . $image_name;
 
-   if($image_name != "")
-   {
-    $path = "../images/category/".$image_name;
+        // Check if the file exists before attempting to remove it
+        if (file_exists($path)) {
+            $remove = unlink($path);
 
-    //remove img
-    $remove = unlink($path);
-
-    //if failed to remove img stop the process
-    if($remove==false)
-    {
-        $_SESSION['remove'] = "<div class='error text-center'> Failed to remove category image.</div>";
-
-        //redirect to manage category
-        header('location:'.SITEURL. 'admin/manage-category.php');
-        //stop the process
-        die();
-
+            if ($remove == false) {
+                $_SESSION['remove'] = "<div class='error text-center'>Failed to remove category image.</div>";
+                header('location:' . SITEURL . 'admin/manage-category.php');
+                die();
+            }
+        } else {
+            // The file doesn't exist, so you may want to add a message or log this event.
+        }
     }
-    
-   }
 
-}
-else {
-    //redirect
-    header('location:'.SITEURL. 'admin/manage-category.php');
+    $sql = "DELETE FROM tbl_category WHERE id=$id";
+    $res = mysqli_query($conn, $sql);
+
+    if ($res == true) {
+        $_SESSION['delete'] = "<div class='success text-center'>Category image Deleted Successfully.</div>";
+        header('location:' . SITEURL . 'admin/manage-category.php');
+    } else {
+        $_SESSION['delete'] = "<div class='error text-center'>Category image not Deleted Successfully.</div>";
+        header('location:' . SITEURL . 'admin/manage-category.php');
+    }
+} else {
+    header('location:' . SITEURL . 'admin/manage-category.php');
 }
 ?>
