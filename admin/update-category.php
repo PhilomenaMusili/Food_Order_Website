4e32f123655aea773f0d08389e0e27970b5c4b9e
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-06 13:53:47 +0300

    another commit

--- admin/update-category.php
+++ admin/update-category.php
@@ -4,5 +4,28 @@
         <h1>Update Category</h1>
 
         <br><br><br>
+        <form action="" method="POST" enctype="multipart/form data">
+            <table class="tbl-30">
+                <tr>
+                    <td>Title</td>
+                    <td>
+                        <input type="text" name="title" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Curent $image_name</td>
+                    <td>
+                        image displayed here
+                    </td>
+                </tr>
+                <tr>
+                    <td>New Image: </td>
+                    <td>
+                        <input type="file" name="image">
+                    </td>
+                </tr>
+            </table>
+      </form>
+</div>
 
 <?php include('partials/footer.php');?>
