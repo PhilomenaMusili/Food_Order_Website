8ef8620246fc0fc6f0c34b95324f8a1b04b91f01
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-06 13:46:25 +0300

    another commit

---
+++ admin/update-category.php
@@ -0,0 +1,8 @@
+<?php include('partials/menu.php');?>
+<div class="main_content">
+    <div class="wrapper">
+        <h1>Update Category</h1>
+
+        <br><br><br>
+
+<?php include('partials/footer.php');?>
