e2fb3a51e7336d02fef19726c4584aecc8b4d293
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-07 12:44:13 +0300

    First commit

---
+++ admin/add-food.php
@@ -0,0 +1,13 @@
+<?php include('partials/menu.php') ?>
+
+
+<div class="main_content">
+    <div class="wrapper">
+        <h1> Add Food</h1>
+
+        <br><br><br>
+    </div>
+</div>
+
+<?php include('partials/footer.php') ?>
+
