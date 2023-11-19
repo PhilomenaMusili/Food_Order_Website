29e226ba31c562a469c0adf27211afe844414fb4
PhilomenaMusili <philomenamusili@gmail.com>
2023-11-19 17:57:04 +0300

    commit

--- admin/login.php
+++ admin/login.php
@@ -1,79 +1,92 @@
 <?php include('../config/constants.php'); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login page</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-<body>
-    <div class="login">
-        <h1 class="text-center">Login</h1>
-        <br><br>
+<html>
+    <head>
+        <title>Login - Food Order System</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
         
-        <?php
-        if(isset($_SESSION['login']))
-            {
-                echo $_SESSION['login'];
-                unset($_SESSION['login']);
+        <div class="login">
+            <h1 class="text-center">Login</h1>
+            <br><br>
 
-            }
-            if(isset($_SESSION['no-login-msg']))
-            {
-                echo $_SESSION['no-login-msg'];
-                unset($_SESSION['no-login-msg']);
+            <?php 
+                if(isset($_SESSION['login']))
+                {
+                    echo $_SESSION['login'];
+                    unset($_SESSION['login']);
+                }
 
-            }
-        ?>
-        <br><br>
+                if(isset($_SESSION['no-login-message']))
+                {
+                    echo $_SESSION['no-login-message'];
+                    unset($_SESSION['no-login-message']);
+                }
+            ?>
+            <br><br>
 
-        <form action="" method="POST" class="text-center">
-            Username:<br>
-            <input type="text" name="username" placeholder="Enter your Username"><br><br>
-            Password:<br>
-            <input type="password" name="password" placeholder="Enter your password"><br><br>
-            <input type="submit" name="submit" value="Login" class="btn-primary"><br><br>
+            <!-- Login Form Starts HEre -->
+            <form action="" method="POST" class="text-center">
+            Username: <br>
+            <input type="text" name="username" placeholder="Enter Username"><br><br>
 
-        </form>
+            Password: <br>
+            <input type="password" name="password" placeholder="Enter Password"><br><br>
 
-        <p class="text-center">Created by - <a href="philomenamusli2@gmail.com">Philomena Kyalo</a></p>
-    </div>
-</body>
+            <input type="submit" name="submit" value="Login" class="btn-primary">
+            <br><br>
+            </form>
+            <!-- Login Form Ends HEre -->
+
+            <p class="text-center">Created By - <a href="www.vijaythapa.com">Vijay Thapa</a></p>
+        </div>
+
+    </body>
 </html>
 
-<?php
-if(isset($_POST['submit']))
-{
-    $username= $_POST['username'];
-    $password= md5($_POST['password']);
+<?php 
 
-    $sql = "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password' ";
+    //CHeck whether the Submit Button is Clicked or NOt
+    if(isset($_POST['submit']))
+    {
+        //Process for Login
+        //1. Get the Data from Login form
+        // $username = $_POST['username'];
+        // $password = md5($_POST['password']);
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        
+        $raw_password = md5($_POST['password']);
+        $password = mysqli_real_escape_string($conn, $raw_password);
 
-    $res=mysqli_query($conn, $sql);
+        //2. SQL to check whether the user with username and password exists or not
+        $sql = "SELECT * FROM tbl_admin WHERE username='$username' AND password='$password'";
 
-    //count rows to check if user exist or not
-    $count =mysqli_num_rows($res);
+        //3. Execute the Query
+        $res = mysqli_query($conn, $sql);
 
-    //check whether have admin data or not
-    if($count==1){
-        //USER AVAILABLE
-        $_SESSION['login'] = "<div class='success text-center'>Login Successful.</div>";
-        $_SESSION['user'] = $username;
-        //redirect
-        header('location:'.SITEURL.'admin/');
+        //4. COunt rows to check whether the user exists or not
+        $count = mysqli_num_rows($res);
+
+        if($count==1)
+        {
+            //User AVailable and Login Success
+            $_SESSION['login'] = "<div class='success'>Login Successful.</div>";
+            $_SESSION['user'] = $username; //TO check whether the user is logged in or not and logout will unset it
+
+            //REdirect to HOme Page/Dashboard
+            header('location:'.SITEURL.'admin/');
+        }
+        else
+        {
+            //User not Available and Login FAil
+            $_SESSION['login'] = "<div class='error text-center'>Username or Password did not match.</div>";
+            //REdirect to HOme Page/Dashboard
+            header('location:'.SITEURL.'admin/login.php');
+        }
 
 
     }
-    else{
-        //USER NOT AVAILABLE
-        $_SESSION['login'] = "<div class='error text-center'>Login Failed.Username or Password did not match</div>";
-        //redirect
-        header('location:'.SITEURL.'admin/login.php');
-    }
-
-
-}
 
 ?>
